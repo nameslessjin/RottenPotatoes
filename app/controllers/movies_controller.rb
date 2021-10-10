66da9b7cfea4bd62bef8ddc80a27c77d1c6f554e
Jinsen Wu <jw4157@columbia.edu>
2021-10-10 16:39:26 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +7,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
+    
+
+    if (params["ratings"])
+      @ratings_to_show = params["ratings"].keys
+    end
+
+    
+    @movies = Movie.with_ratings(@ratings_to_show)
+    
+    
+    
+    #"ratings"=>{"PG"=>"1", "PG-13"=>"1"}, "commit"=>"Refresh", "controller"=>"movies", "action"=>"index"
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,28 @@
 class Movie < ActiveRecord::Base
+ 
+  def self.all_ratings
+    return ['G','PG','PG-13','R']
+  end
+  
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  # if ratings_list is nil, retrieve ALL movies
+    
+    if (ratings_list.length == 0)
+      return Movie.all
+    else
+      list = Movie.where(
+        "rating in (:ratings_list)",
+        {ratings_list: ratings_list}
+      ).all
+      
+      return list
+    end
+    
+    return Movie.all
+    
+  end
+  
+  
 end
